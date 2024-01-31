b7e5ff43d4960ddba8180da9099445159f79f8df
Duy Hung Tran <hung.dtrn@gmail.com>
2024-01-31 22:27:29 +1100

    update data & add deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-REGION=australia-southeast2
+REGION=australia-southeast1
 PROJECT=anz-ml-engineer
+PROJECT_ID=977770941525
 SOURCE_DIR="."
-glcoud run deploy data_insight --project $PROJECT  --region=$REGION --allow-unauthenticated \
+
+gcloud projects add-iam-policy-binding ${PROJECT} \
+    --member=serviceAccount:${PROJECT_ID}-compute@developer.gserviceaccount.com \
+    --role=roles/secretmanager.secretAccessor \
+
+gcloud secrets add-iam-policy-binding OPENAI_API_KEY \
+    --member="serviceAccount:${PROJECT_ID}-compute@developer.gserviceaccount.com \
+    --role="roles/secretmanager.secretAccessor"
+
+
+gcloud run deploy data_insight --project $PROJECT  --region=$REGION --allow-unauthenticated \
     --update-secrets=OPENAI_API_KEY=OPENAI_API_KEY:latest \
     --set-env-vars "MODEL=gpt-3.5-turbo" \
     --set-env-vars SERVER_URL=http://0.0.0.0:3000 \
     --source=$SOURCE_DIR \
+
